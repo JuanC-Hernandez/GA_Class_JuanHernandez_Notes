30a707225ba4ae7970440ffe183a72e5a0e9da62
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2024-01-08 19:18:46 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GA_Class_JuanHernandez_Notes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
